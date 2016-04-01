3e04c8a6e68984e3e522d9401a570c502f2561e2
Nathaniel Jones <nljones@mit.edu>
2016-04-01 17:02:35 -0400

    Added gencumsky

---
+++ src/daysim/gencumsky.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <stdio.h>
+
+#ifdef __CPLUSPLUS
+extern "C" {
+#endif
+
+int gencumsky(int argc, char* argv[], FILE * output, FILE * error);
+
+#ifdef __CPLUSPLUS
+}
+#endif
